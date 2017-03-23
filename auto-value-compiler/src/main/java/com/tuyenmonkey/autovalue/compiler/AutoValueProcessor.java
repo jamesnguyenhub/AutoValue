9349570556ad91a887465f1583ef2a6a264df241
Tuyen Nguyen <nntuyen92@gmail.com>
2017-03-23 14:15:16 +0700

    Compile project

--- auto-value-compiler/src/main/java/com/tuyenmonkey/autovalue/compiler/AutoValueProcessor.java
+++ auto-value-compiler/src/main/java/com/tuyenmonkey/autovalue/compiler/AutoValueProcessor.java
@@ -1,8 +1,19 @@
 package com.tuyenmonkey.autovalue.compiler;
 
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.TypeElement;
+
 /**
  * Created by Tuyen Monkey on 3/23/17.
  */
 
-public class AutoValueProcessor {
+@SupportedAnnotationTypes("com.tuyenmonkey.autovalue.annotation.AutoValue")
+public class AutoValueProcessor extends AbstractProcessor {
+  @Override
+  public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+    return false;
+  }
 }
